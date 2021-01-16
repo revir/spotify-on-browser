dc657283159b1f496349a3b57dd537071a400b63
river <revir.qing@gmail.com>
2021-01-16 21:19:33 +0800

    feat: pack.sh

---
+++ pack.sh
@@ -0,0 +1,17 @@
+name="spotify-on-chrome.zip"
+srcname="spotify-on-chrome-src.zip" 
+
+npm run build
+
+rm -f ${name}
+rm -f ${srcname}
+
+echo "pack to ${name}: "
+cd build/
+zip -r ${name} . 
+mv ${name} ../
+cd ../
+
+echo
+echo "pack to ${srcname}: "
+zip -x '*.DS_Store*' -x '*build/*' -x '*readme_images/*' -x '*.git*' -x 'test/*' -x 'node_modules/*' -x "bower_components/*" -x "build.*" -x "${name}" -r ${srcname} .
